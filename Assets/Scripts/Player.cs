d05b754aec573af2dfc578a86f8acd9e1402f7d3
Trozeman <trozeman@gmail.com>
2018-01-26 22:26:29 +0200

    Presets

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour
+{
 
+	public int PlayerHealth;
+
+	public bool enemy;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		PlayerHealth = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (enemy = true)
+		{
+			PlayerHealth -= 1;
+		}
 	}
 }
