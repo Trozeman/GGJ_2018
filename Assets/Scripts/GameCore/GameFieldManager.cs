a1e23352325e44469e9c7d4a2d4f360733d6c76c
Mykhailo.Zhovnach <mykhailo.zhovnach@gmail.com>
2018-01-27 00:03:55 +0200

    First core game mechanic

---
+++ Assets/Scripts/GameCore/GameFieldManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldManager : MonoBehaviour
+{
+    public Vector3 Size;
+    private GameField _gameField;
+
+	// Use this for initialization
+	void Awake () {
+        _gameField = new GameField();
+        _gameField.Init(Size);
+        _gameField.GenerateField(GameBalanceConst.InitalPointsCount);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
