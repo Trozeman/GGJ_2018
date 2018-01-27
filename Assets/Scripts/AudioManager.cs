71f4bb853156349af130becb28694f5a4fae43fd
Trozeman <trozeman@gmail.com>
2018-01-27 23:56:28 +0200

    Sounds MP3

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿/*
+using  UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class AudioManager : MonoBehaviour
+{
+
+	public Sounds[] _Sounds;
+
+	private void Awake()
+	{
+		foreach (Sound in _Sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			
+			s.source.volume = s.
+		}
+	}
+}
+*/
---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,30 @@
+﻿/*
+using  UnityEngine.Audio;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour {
+
+	public string _Name;
+	
+	
+	public AudioClip _Audio;
+	
+	[Range(0f, 1f)]
+	
+	public float _Volume;
+
+	[HideInInspector]
+	public AudioSource _Source;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+*/
