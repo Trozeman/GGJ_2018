3569f120c7074d5ae3d38710b87dc63e14009fbc
Mykhailo.Zhovnach <mykhailo.zhovnach@gmail.com>
2018-01-27 14:08:05 +0200

    colors

--- Assets/Scripts/ShowPoints.cs
+++ Assets/Scripts/ShowPoints.cs
@@ -7,18 +7,18 @@ public class ShowPoints : MonoBehaviour
 	public GameObject Prefab;
 
     private GameField _data;
-    private List<GameObject> _stations;
+    private List<StationView> _stations;
 
     // Use this for initialization
     void Start ()
 	{
         _data = GetComponent<GameFieldManager>().GetGameField();
-        _stations = new List<GameObject>();
+        _stations = new List<StationView>();
 
         foreach (var station in _data.Points)
 		{
             Vector3 pos = new Vector3(station.x, station.y, 0.0f);
-            GameObject stationObj = Instantiate(Prefab, pos, Quaternion.identity, gameObject.transform);
+            StationView stationObj = Instantiate(Prefab, pos, Quaternion.identity, gameObject.transform).GetComponent<StationView>();
             _stations.Add(stationObj);
             LineRenderer lineRend = stationObj.GetComponent<LineRenderer>();
             List<Vector3> _lines = new List<Vector3>();
@@ -40,6 +40,7 @@ public class ShowPoints : MonoBehaviour
         {
             Station stationData = _data.Points[index];
             station.transform.localScale = Vector3.one * (1.0f + stationData.Radiation / 10.0f);
+            station.PointSprite.color = new Color(Mathf.Min(1.0f, stationData.Radiation * 0.01f) , 1/(stationData.Radiation + 0.1f) , 0);
             index++;
         }
 
---
+++ Assets/Scripts/StationView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationView : MonoBehaviour {
+
+    public SpriteRenderer PointSprite;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}	
+}
