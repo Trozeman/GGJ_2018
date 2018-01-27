f4015cf5b05baddb8b68b4690ce13ce0ea1cd252
Zhdankin <antonzhdankin@gmail.com>
2018-01-27 17:10:58 +0200

    changed game balance

--- Assets/Scripts/GameCore/GameBalanceConst.cs
+++ Assets/Scripts/GameCore/GameBalanceConst.cs
@@ -6,11 +6,12 @@ public class GameBalanceConst
 {
     // game parameters
     public static float Intensity = 10.0f;
-    public static float IntensityToRadiusRatio = 0.1f;
+    public static float IntensityToRadiusRatio = 0.05f;
     //public static int SpreadDepth = 1;
-    public static int SpreadSize = 3;
+    public static int SpreadSize = 2;
     public static float RadiationAbsorption = 2;
-    public static float GlobalCensorAbsorption = 2.0f;
+
+    public static float GlobalCensorAbsorption = 3.70f;
 
     public static int InitalPointsCount = 100;
 }
