7d5f572cb1f0edeaa21fa7a013de1eaa106167fc
Zhdankin <antonzhdankin@gmail.com>
2018-01-27 15:07:36 +0200

    changes global game variables

--- Assets/Scripts/GameCore/GameBalanceConst.cs
+++ Assets/Scripts/GameCore/GameBalanceConst.cs
@@ -7,9 +7,9 @@ public class GameBalanceConst
     // game parameters
     public static float Intensity = 10.0f;
     //public static int SpreadDepth = 1;
-    public static int SpreadSize = 0;
-    public static float RadiationAbsorption = Intensity;
-    public static float GlobalCensorAbsorption = 5.0f;
+    public static int SpreadSize = 3;
+    public static float RadiationAbsorption = 2;
+    public static float GlobalCensorAbsorption = 2.0f;
 
     public static int InitalPointsCount = 100;
 }
