20ab7ad79f8d34209a3101b171250eaea6af7109
Zhdankin <antonzhdankin@gmail.com>
2018-01-28 04:59:03 +0200

    changed global game balance

--- Assets/Scripts/GameCore/GameBalanceConst.cs
+++ Assets/Scripts/GameCore/GameBalanceConst.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class GameBalanceConst
 {
-    public static int MaximumTransitions = 400;
+    public static int MaximumTransitions = 800; //400
     // game parameters
     public static float Intensity = 10.0f;
     public static float IntensityToRadiusRatio = 0.05f;
     //public static int SpreadDepth = 1;
-    public static int SpreadSize = 2;
+    public static int SpreadSize = 4; //2
     public static float RadiationAbsorption = 2;
     public static float GlobalCensorAbsorption = 3.70f;
-    public static int InitalPointsCount = 50;
+    public static int InitalPointsCount = 150; //100
     public static float AutoTransmitButtonInterval = 0.6f;
     public static float MaxIntensityToDropDown = 250;
 }
