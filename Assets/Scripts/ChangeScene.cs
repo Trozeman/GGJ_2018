795cf3897ac07a3b182c9ac8262299cab5e9c859
Trozeman <trozeman@gmail.com>
2018-01-28 06:06:16 +0200

    Pause, Souns, Scene changer

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Debug = System.Diagnostics.Debug;
+
+public class ChangeScene : MonoBehaviour
+{
+
+	public int _ID;
+
+	public void Change(int i)
+	{
+		if (i < 0)
+		{
+			Application.Quit();
+		}
+		else
+		{
+			SceneManager.LoadScene(i);
+		}
+	}
+
+
+	// Use this for initialization
+
+
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+	private bool _Pause = false;
+
+	public void TogglePause()
+	{
+		if (_Pause)
+		{
+			Time.timeScale = 1;
+			_Pause = !_Pause;
+		}
+		else
+		{
+			Time.timeScale = 0;
+			_Pause = !_Pause;
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			TogglePause();
+		}
+	}
+}
